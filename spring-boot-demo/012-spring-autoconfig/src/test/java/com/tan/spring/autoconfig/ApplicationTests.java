f7420640b32245e21d707fdc9d2c95a0050a4d6f
tanjezh <tan19972100@163.com>
2024-05-04 19:50:52 +0800

    容器中的 bean 加载，通过注解 @Component 或者 @Bean 放入容器，根据自定义注解方式加载到容器中。第三方依赖的对象不能放入容器

---
+++ spring-boot-demo/012-spring-autoconfig/src/test/java/com/tan/spring/autoconfig/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tan.spring.autoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
