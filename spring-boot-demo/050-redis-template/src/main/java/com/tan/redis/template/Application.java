2f5f53d46d466518c58ff64a211964aeb719b3b0
tanjezh <tan19972100@163.com>
2024-09-23 23:14:07 +0800

    redis 的 hyperloglog 使用案例

--- spring-boot-demo/050-redis-template/src/main/java/com/tan/redis/template/Application.java
+++ spring-boot-demo/050-redis-template/src/main/java/com/tan/redis/template/Application.java
@@ -1,10 +1,15 @@
 package com.tan.redis.template;
 
+import com.tan.redis.template.demo.HyperLoglogTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    public Application(HyperLoglogTest hyperLoglogTest){
+        hyperLoglogTest.test();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
